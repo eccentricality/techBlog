b8232ff619519cb9479e2e602899649c9dd08526
eccentricality <youngmin13@gmail.com>
2021-07-31 10:26:01 -0400

    created initial server.js imported required techs

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const routes = require('./routes/index');
+const sequelize = require('sequelize');
+const path = require('path');
+
+// helper
+const helpers = require('./helpers');
+
+// handlebars  
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({helpers});
+
+// session connection to sequelize db
+const session = require('express-session');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+// creates the session
+const sess = {
+    secret: 'the unknown',
+    cookie: { originalMaxAge: 60000 },
+    saveUninitialized: false,
+    resave: false,
+    store: new SequelizeStore({
+        db: sequelize,
+    })
+};
+
+app.use(session(sess));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// default engine set to handlebars
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// activate routes
+app.use('/', routes);
+
+// db server connection
+sequelize.sync({force: false}).then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server listening on port ${PORT}`);
+    });
+});
